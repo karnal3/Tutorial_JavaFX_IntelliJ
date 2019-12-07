c563a90c5b91e9c2eb01e09be2206b69687fdba3
karnal3 <diwad1998@gmail.com>
2019-12-07 15:45:58 +0100

    Learn how to add Events in Tutorial2

---
+++ src/com/karnal3/Tutorial3/Main.java
@@ -0,0 +1,42 @@
+package com.karnal3.Tutorial3;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Title of the window");
+        button = new Button();
+        button.setText("Click me!");
+
+        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if(actionEvent.getSource()==button){
+            System.out.println("Write to console when button clicked");
+        }
+    }
+}
