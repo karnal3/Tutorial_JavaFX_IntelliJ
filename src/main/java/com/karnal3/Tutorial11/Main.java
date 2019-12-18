f28fa747c3d058a396f0b692ca46049eedbd7ed7
karnal3 <diwad1998@gmail.com>
2019-12-18 23:36:43 +0100

    I ordered Tuna, end of tutorial 11 checkobox

--- src/main/java/com/karnal3/Tutorial11/Main.java
+++ src/main/java/com/karnal3/Tutorial11/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,15 +21,34 @@ public class Main extends Application {
 
         window=primaryStage;
         window.setTitle("Checkbox example");
+
+        CheckBox box1 = new CheckBox("Bacon");
+        CheckBox box2 = new CheckBox("Tuna");
+        box2.setSelected(true);
+
         button = new Button("Order now!");
+
+        button.setOnAction(e -> handleOptions(box1,box2));
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll(button);
+        layout.getChildren().addAll(button,box1,box2);
 
         scene = new Scene(layout,300,250);
         window.setScene(scene);
         window.show();
+    }
+
+    private void handleOptions(CheckBox box1, CheckBox box2){
+        String message = "User order : \n";
+
+        if(box1.isSelected()){
+            message += "Bacon\n";
+        }
+        if(box2.isSelected()){
+            message += "Tuna\n";
+        }
+        System.out.println(message);
     }
 
     public static void main(String[] args) {
