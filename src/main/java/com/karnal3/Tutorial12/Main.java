58601d999418f8ed54d8195045be8cd1b0dd4729
karnal3 <diwad1998@gmail.com>
2019-12-18 23:45:48 +0100

    choice boxes

--- src/main/java/com/karnal3/Tutorial12/Main.java
+++ src/main/java/com/karnal3/Tutorial12/Main.java
@@ -1,16 +1,54 @@
 package com.karnal3.Tutorial12;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+    Stage window;
+    Scene scene;
+    Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        window=primaryStage;
+        window.setTitle("Checkbox example");
+
+        button = new Button("Click me!");
+
+        ChoiceBox<String> choisceBox = new ChoiceBox<>();
+
+        choisceBox.getItems().add("Apples");
+        choisceBox.getItems().add("Bananas");
+        choisceBox.getItems().addAll("Graple","Ham","Meatballs");
+        choisceBox.setValue("Apples");
+
+        button.setOnAction(e -> getChoisce(choisceBox));
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20));
+        layout.getChildren().addAll(button,choisceBox);
+
+        scene = new Scene(layout,300,250);
+        window.setScene(scene);
+        window.show();
+    }
+
+
+    private void getChoisce(ChoiceBox<String> choiceBox){
+        String food = choiceBox.getValue();
+        System.out.println(food);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
