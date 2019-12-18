4c9a8094d769e2ba9c181bd082f4699d47960dad
karnal3 <diwad1998@gmail.com>
2019-12-18 23:38:55 +0100

    Ready for tutorial nr 12

---
+++ src/main/java/com/karnal3/Tutorial12/Main.java
@@ -0,0 +1,16 @@
+package com.karnal3.Tutorial12;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
