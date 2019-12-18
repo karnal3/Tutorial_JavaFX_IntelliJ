a53d1ac7d5359880b8752632e6d856d931a2cd8a
karnal3 <diwad1998@gmail.com>
2019-12-18 23:49:14 +0100

    choice boxes and listeners for selection changes

---
+++ src/main/java/com/karnal3/Tutorial13/Main.java
@@ -0,0 +1,55 @@
+package com.karnal3.Tutorial13;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+
+    Stage window;
+    Scene scene;
+    Button button;
+
+    @Override
+    public void start(Stage primaryStage)  {
+
+        window=primaryStage;
+        window.setTitle("Checkbox example");
+
+        button = new Button("Click me!");
+
+        ChoiceBox<String> choisceBox = new ChoiceBox<>();
+
+        choisceBox.getItems().add("Apples");
+        choisceBox.getItems().add("Bananas");
+        choisceBox.getItems().addAll("Graple","Ham","Meatballs");
+        choisceBox.setValue("Apples");
+
+        button.setOnAction(e -> getChoisce(choisceBox));
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20));
+        layout.getChildren().addAll(button,choisceBox);
+
+
+        scene = new Scene(layout,300,250);
+        window.setScene(scene);
+        window.show();
+    }
+
+
+    private void getChoisce(ChoiceBox<String> choiceBox){
+        String food = choiceBox.getValue();
+        System.out.println(food);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
